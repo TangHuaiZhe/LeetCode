f1d11a1104f903aea0d0c167a688eb3d321351cb
Tang HuaiZhe <tanghuaizhe@me.com>
2019-11-20 18:12:23 +0800

    update

--- 5_LongestPalindromicSubstring.kt
+++ 5_LongestPalindromicSubstring.kt
@@ -41,13 +41,13 @@ fun longestPalindrome(s: String?): String {
   if (s == null || s.isEmpty()) return ""
   var start = 0
   var end = 0
-  for (i in 0 until s.length) {
-    val len1 = expandAroundCenter(s, i, i);
-    val len2 = expandAroundCenter(s, i, i + 1);
+  for (middle in 0 until s.length) {
+    val len1 = expandAroundCenter(s = s, left = middle, right = middle);
+    val len2 = expandAroundCenter(s = s, left = middle, right = middle + 1);
     val len = max(len1, len2);
     if (len > end - start) {
-      start = i - (len - 1) / 2;
-      end = i + len / 2;
+      start = middle - (len - 1) / 2;
+      end = middle + len / 2;
     }
   }
   return s.substring(start, end + 1);
@@ -55,7 +55,7 @@ fun longestPalindrome(s: String?): String {
 
 fun expandAroundCenter(s: String, left: Int, right: Int): Int {
   var L = left
-  var R: Int = right
+  var R = right
   while (L >= 0 && R < s.length && s[L] == s[R]) {
     L--
     R++
