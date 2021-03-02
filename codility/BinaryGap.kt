3d3853a8f1349a95e8e6a612554b7d5ed2b71159
Tang HuaiZhe <tanghuaizhe@me.com>
2021-03-02 11:17:21 +0800

    BinaryGap

---
+++ codility/BinaryGap.kt
@@ -0,0 +1,46 @@
+package codility
+
+/**
+ * author: i530643
+ * created on: 3/1/21 2:26 PM
+ * description:
+ */
+
+fun solution(N: Int): Int {
+  // write your code in Kotlin
+  val binary = decToBinary(N)
+  return scanBinaryGap(binary)
+}
+
+fun decToBinary(decNum: Int): String {
+  var decNumLocal = decNum
+  val result = StringBuilder()
+  while (decNumLocal > 0) {
+    val r = decNumLocal % 2
+    decNumLocal /= 2
+    result.append(r)
+  }
+  return result.reverse().toString()
+}
+
+fun scanBinaryGap(binaryString: String):Int {
+  var left = 0
+  var max = 0
+  binaryString.forEachIndexed { index, c ->
+    if (c == '1') {
+      val gap = index - left - 1
+      if (gap > max && gap >= 1) {
+        max = gap
+      }
+      left = index
+    }
+  }
+  return max
+}
+
+fun main() {
+  val result = solution(15)
+  val result1 = solution(1041)
+  println(result)
+  println(result1)
+}
